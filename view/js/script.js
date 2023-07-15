e6b064b702d1572b7b6ffa06cdfe6d4bbfc75d4d
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-07-14 22:02:04 -0300

    Add files via upload

---
+++ view/js/script.js
@@ -0,0 +1,23 @@
+const botao = document.querySelector('.botao');
+const imagem = document.querySelector('.img-botao');
+const header = document.getElementById('header');
+const nav = document.getElementById('nav');
+
+function abrirMenu() {
+    
+    nav.classList.toggle('ativo');            
+
+    if (nav.classList.contains('ativo')) {
+        nav.classList.add('abrindoMenu');
+        imagem.src = "view/img/menu-fechar.png";    
+    } else {    
+        nav.classList.remove('abrindoMenu');
+        imagem.src = "view/img/menu.png";    
+    }
+
+    setTimeout(() => {
+        nav.classList.remove('ativo');
+        imagem.src = "view/img/menu.png";
+    }, 10000);
+
+}
